43edc02a797f78e3706fda43d0240c2adbc538a0
buckholz <glenn.buckholz@covers.com>
2016-04-29 16:06:55 -0400

    initial commit

---
+++ tomcat/metadata.rb
@@ -0,0 +1,10 @@
+name 'tomcat'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures tomcat'
+long_description 'Installs/Configures tomcat'
+version '0.1.1'
+
+depends 'yum-epel'
+depends 'java'
---
+++ tomcat/recipes/default.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: tomcat
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+include_recipe 'yum-epel'
+include_recipe 'java'
+
+package 'tomcat' do
+  action :install
+end
+
+template "/etc/tomcat/server.xml" do
+  source "server.xml.erb"
+  action :create
+end
+
+template "/usr/share/tomcat/conf/server.xml" do
+  source "server.xml.erb"
+  action :create
+end
+
+service 'tomcat' do
+  action [ :enable, :start ]
+end
