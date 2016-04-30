9fa2298d17cf55e470fd8488d4f627fe032bd620
buckholz <glenn.buckholz@coveros.com>
2016-04-30 09:35:30 -0400

    format serverspec tests

--- cookbooks/tomcat/test/integration/helpers/serverspec/spec_helper.rb
+++ cookbooks/tomcat/test/integration/helpers/serverspec/spec_helper.rb
@@ -1,4 +1,12 @@
 require 'serverspec'
+require ‘yarjuf’
+
+RSpec.configure do |c|
+
+    c.formatter = ‘JUnit’
+
+end
+
 
 if (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM).nil?
   set :backend, :exec
