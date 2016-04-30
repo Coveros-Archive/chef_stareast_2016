1891d93e52e34bb4014a8b51d6b7c8ea255b0dd2
buckholz <glenn.buckholz@coveros.com>
2016-04-30 08:42:24 -0400

    created sample tests

--- cookbooks/tomcat/spec/unit/recipes/default_spec.rb
+++ cookbooks/tomcat/spec/unit/recipes/default_spec.rb
@@ -13,8 +13,29 @@ describe 'tomcat::default' do
       runner.converge(described_recipe)
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'installs tomcat' do
+      expect { chef_run }.to install_package('tomcat')
     end
+    it 'creates server.xml in conf' do
+      expect(chef_run).to create_file('/usr/share/tomcat/conf/server.xml').with(
+        user:   'tomcat',
+        group:  'tomcat'
+        )
+    end 
+    it 'creates server.xml in etc' do
+      expect(chef_run).to create_file('/etc/tomcat/server.xml').with(
+        user:   'tomcat',
+        group:  'tomcat'
+        )
+    end 
+    
+    it 'enables a service tomcat action' do
+      expect(chef_run).to enable_service('tomcat')
+    end
+    
+    it 'starts a service tomcat action' do
+      expect(chef_run).to start_service('tomcat')
+    end
+    
   end
 end
